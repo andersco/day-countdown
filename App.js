0dc43fe58a2cb06c070ff0d9cf8e563084accb9d
Scott Anderson <scott.anderson@autodesk.com>
2019-02-22 16:24:25 -0800

    Add navigation and styling

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import React from 'react';
 import EventList from './EventList';
+import EventForm from './EventForm';
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <EventList/>
-    );
+const AppNavigator = createStackNavigator({
+  list: {
+    screen: EventList,
+    navigationOptions: () => ({
+      title: 'Your Events'
+    })
+  },
+  form: {
+    screen: EventForm,
+    navigationOptions: () => ({
+      title: 'Add New Event'
+    })
   }
-}
+});
+
+export default createAppContainer(AppNavigator);
--- EventList.js
+++ EventList.js
@@ -1,6 +1,14 @@
-import React, {Component} from 'react';
-import {FlatList, Text} from 'react-native';
+import React, { Component } from 'react';
+import { FlatList, StyleSheet } from 'react-native';
 import EventCard from './EventCard';
+
+const styles = StyleSheet.create({
+    list: {
+        flex: 1,
+        paddingTop: 5,
+        backgroundColor: '#F3F3F3'
+    },
+});
 
 class EventList extends Component {
     state = {
@@ -8,18 +16,27 @@ class EventList extends Component {
     }
 
     componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                events: this.state.events.map(evt => ({
+                    ...evt,
+                    timer: Date.now(),
+                })),
+            });
+        }, 1000);
         const events = require('./db.json').events.map(e => ({
             ...e,
             date: new Date(e.date)
         }));
-        this.setState({events});
+        this.setState({ events });
     }
 
     render() {
         return (
             <FlatList
+                style={styles.list}
                 data={this.state.events}
-                renderItem={({item}) => <EventCard event={item}/>}
+                renderItem={({ item }) => <EventCard event={item} />}
                 keyExtractor={item => item.id}
             />
         );
